7eb5aacbc360bfd9a1723e1ec2ff37708a657e35
ShawarmaExpress <jose.rafael.molina.montano@gmail.com>
2023-11-15 13:41:52 -0500

    metodos de actualizacion y creacion construidos

--- pages/api/database.js
+++ pages/api/database.js
@@ -3,7 +3,7 @@ const singersList = [
   {
     id: "1",
     name: "Taylor Momsen",
-    birthday: "26/07/1993",
+    birthday: "1993/07/26",
     nationality: "United States",
     musicType: ["Punk", "Alternative Rock", "Hard Rock"],
     image: "https://people.com/thmb/vWKDuC-RRPN62VtPxFocS2rNFjA=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(799x479:801x481)/taylor-momsen-2000-6dda5a8b79fa40ff9cf53621e388af94.jpg",
@@ -16,7 +16,7 @@ const singersList = [
   {
     id: "2",
     name: "Tatiana Shmaylyuk",
-    birthday: "15/03/1987",
+    birthday: "1987/03/15",
     nationality: "Ukraine",
     musicType: ["Groove Metall"],
     image: "https://i.pinimg.com/originals/ac/e0/08/ace008bc77f062e4b9eb555091dfa189.jpg",
@@ -29,7 +29,7 @@ const singersList = [
   {
     id: "3",
     name: "David Draimank",
-    birthday: "13/03/1973",
+    birthday: "1973/03/13",
     nationality: "United States",
     musicType: ["Nu-Metall", "Heavy Metall", "Hard rock", "Alternative Metall", "Industrial Metall"],
     image: "https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcQcq_BhgkgJcUQW6Cwq1G6Srm2OObxp4nBTE0wizpVVqj4azHxWkmpJwHotZ4dwFsTX",
@@ -42,7 +42,7 @@ const singersList = [
   {
     id: "4",
     name: "Madeleine Liljestam",
-    birthday: "18/09/1990",
+    birthday: "1990/09/18",
     nationality: "Sweden",
     musicType: ["Synphonic Metall"],
     image: "https://dark-divas.com/wp-content/uploads/2020/08/ELEINE-Reviews-3-e1625844051902.jpg",
@@ -55,7 +55,7 @@ const singersList = [
   {
     id: "5",
     name: "Lena Scissorhands",
-    birthday: "22/11/1986",
+    birthday: "1986/11/22",
     nationality: "Moldova",
     musicType: ["Nu Metal", "Metalcore", "Groove Metal", "Melodic Death Metal", "Alternative Metal"],
     image: "https://i.pinimg.com/originals/72/71/10/72711059b851410570c7e0be7887da0a.jpg",
@@ -68,7 +68,7 @@ const singersList = [
   {
     id: "6",
     name: "Rob Zombie",
-    birthday: "12/01/1965",
+    birthday: "1965/01/12",
     nationality: "United States",
     musicType: ["Heavy Metal", "Metal Alternativo", "Metal Industrial", "Groove Metal", "Hard rock", "Nu Metal", "Shock Rock"],
     image: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQyJt6mLxYwAkj6f23W_Q7eXLjkj9Om0Or6NIpk8qzTh_DKm3kT",
@@ -81,7 +81,7 @@ const singersList = [
   {
     id: "7",
     name: "Kvitrafn",
-    birthday: "18/11/1979",
+    birthday: "1979/11/18",
     nationality: "Norway",
     musicType: ["Black Metal", "Heavy Metal", "Thrash Metal", "Folk", "Ambient"],
     image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Wardruna_%40_Roadburn_2015_01.jpg/330px-Wardruna_%40_Roadburn_2015_01.jpg",
@@ -94,7 +94,7 @@ const singersList = [
   {
     id: "8",
     name: "Maria Brink",
-    birthday: "18/12/1977",
+    birthday: "1977/12/18",
     nationality: "United States",
     musicType: ["Heavy Metal", "Metalcore", "Metal Alternativo", "Metal Gótico", "Post-hardcore"],
     image: "https://i.pinimg.com/originals/ed/95/68/ed9568c85bb2ce1390ec27366e0b619b.jpg",
@@ -107,7 +107,7 @@ const singersList = [
   {
     id: "9",
     name: "Ozzy Osbourne",
-    birthday: "3/12/1948",
+    birthday: "1948/12/03",
     nationality: "British",
     musicType: ["Heavy Metal", "Hard Rock"],
     image: "https://m.media-amazon.com/images/M/MV5BNzk3OTYyMzc2Nl5BMl5BanBnXkFtZTcwMTk3NTUyMg@@._V1_FMjpg_UX1000_.jpg",
@@ -120,7 +120,7 @@ const singersList = [
   {
     id: "10",
     name: "2D",
-    birthday: "23/05/1978",
+    birthday: "1978/05/23",
     nationality: "British",
     musicType: ["Grunch"],
     image: "https://dthezntil550i.cloudfront.net/5q/latest/5q1807292022025420003326381/1280_960/8a33426b-abb1-4b7c-9151-6c7a9ee34871.png",
